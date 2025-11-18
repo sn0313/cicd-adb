336a455850807c9d97e96bc6dd74db0151cee4de
Oracle Public Cloud User <opc@jenkins-master-private-instance.cicdprivatesubn.cicdvcn.oraclevcn.com>
2025-11-18 03:48:08 +0000

    add test table 2

---
+++ src/database/dev_user_1/ords/ords.sql
@@ -0,0 +1,60 @@
+
+--
+        
+DECLARE
+  l_roles     OWA.VC_ARR;
+  l_modules   OWA.VC_ARR;
+  l_patterns  OWA.VC_ARR;
+
+BEGIN
+  ORDS.ENABLE_SCHEMA(
+      p_enabled             => TRUE,
+      p_url_mapping_type    => 'BASE_PATH',
+      p_url_mapping_pattern => 'dev_user_1',
+      p_auto_rest_auth      => TRUE);
+
+  ORDS.CREATE_ROLE(
+      p_role_name=> 'oracle.dbtools.role.autorest.DEV_USER_1');
+  ORDS.CREATE_ROLE(
+      p_role_name=> 'oracle.dbtools.role.autorest.any.DEV_USER_1');
+  l_roles(1) := 'oracle.dbtools.autorest.any.schema';
+  l_roles(2) := 'oracle.dbtools.role.autorest.DEV_USER_1';
+  l_patterns(1) := '/metadata-catalog/*';
+
+  ORDS.DEFINE_PRIVILEGE(
+      p_privilege_name => 'oracle.dbtools.autorest.privilege.DEV_USER_1',
+      p_roles          => l_roles,
+      p_patterns       => l_patterns,
+      p_modules        => l_modules,
+      p_label          => 'DEV_USER_1 metadata-catalog access',
+      p_description    => 'Provides access to the metadata catalog of the objects in the DEV_USER_1 schema.',
+      p_comments       => NULL); 
+
+  l_roles.DELETE;
+  l_modules.DELETE;
+  l_patterns.DELETE;
+
+  l_roles(1) := 'SODA Developer';
+  l_patterns(1) := '/soda/*';
+
+  ORDS.DEFINE_PRIVILEGE(
+      p_privilege_name => 'oracle.soda.privilege.developer',
+      p_roles          => l_roles,
+      p_patterns       => l_patterns,
+      p_modules        => l_modules,
+      p_label          => NULL,
+      p_description    => NULL,
+      p_comments       => NULL); 
+
+  l_roles.DELETE;
+  l_modules.DELETE;
+  l_patterns.DELETE;
+
+
+COMMIT;
+
+END;
+/
+
+
+-- sqlcl_snapshot {"hash":"368e6e753b0439e15af5b8c50b344985ceeca5f6","type":"ORDS_SCHEMA","name":"ords","schemaName":"DEV_USER_1","sxml":""}
